350185f4f6537891ee8380925a8a1180bb352d09
Juan <elranchodejuancho12@gmail.com>
2024-04-28 23:16:57 +0200

    gestor refactorizado 1.0

---
+++ Tarea.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Tarea here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tarea
+{
+    // instance variables - replace the example below with your own
+    
+}
